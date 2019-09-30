1f6466f626c6c3eb23b1479115c6ac79f53f1b90
Net-zen <prankster@myttk.ru>
2019-09-30 16:27:46 +0600

    feat:  add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function zeros(expression) {
-  // your solution
+  const factorials = expression.split('*');
+  let denomin = [2, 5];
+  let two = 0,
+    five = 0;
+  factorials.forEach(element => {
+    let single = (element.includes('!!')) ? 2 : 1;
+    element = parseInt(element);
+    denomin.forEach(el => {
+      let i = element;
+      while (i >= el) {
+        if (!(i % el)) {
+          let x = i;
+          do {
+            x /= el;
+            (el === 2) ? two += 1: five += 1;
+          } while (!(x % el));
+        }
+        i -= single;
+      }
+    });
+  });
+  return (two < five) ? two : five;
 }
