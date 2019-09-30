b44f6d75f3587a0e8bdf93b44b84ee2c3d499f74
Net-zen <prankster@myttk.ru>
2019-09-30 16:55:10 +0600

    refactor: minor optimization

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 module.exports = function zeros(expression) {
   const factorials = expression.split('*');
-  let denomin = [2, 5];
-  let two = 0,
-    five = 0;
+  let denomin = [2, 5], two = 0, five = 0;
   factorials.forEach(element => {
     let single = (element.includes('!!')) ? 2 : 1;
     element = parseInt(element);
     denomin.forEach(el => {
-      let i = element;
-      while (i >= el) {
+      for (let i = element; i >= el; i -= single) {
         if (!(i % el)) {
           let x = i;
-          do {
+          while (!(x % el)) {
             x /= el;
             (el === 2) ? two += 1: five += 1;
-          } while (!(x % el));
+          }
         }
-        i -= single;
       }
     });
   });
